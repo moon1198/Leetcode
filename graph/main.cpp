5de1f9be235e691046e743ce51f977ffeefc5148
Jang <2235908312@qq.com>
2024-03-26 20:09:35 +0800

    Complete the code caprice once

--- graph/main.cpp
+++ graph/main.cpp
@@ -5,5 +5,6 @@ int main() {
     vector<vector<char>> board = {{'O','O','O'},{'O','O','O'},{'O','O','O'}};
     vector<vector<int>> grid = {{0,1,0,0},{1,1,1,0},{0,1,0,0},{1,1,0,0}};
     vector<vector<int>> edges = {{1,2},{2,3},{3,4},{4,1},{1,5}};
+    printf("x=%d y=%d", 3);
     return 0;
 }
